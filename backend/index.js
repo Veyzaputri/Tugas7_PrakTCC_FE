d95ee8ed31b65cb7c4b3dfd5dfb8b773ed19365a
Veyzaputri <129712217+Veyzaputri@users.noreply.github.com>
2025-05-19 22:16:37 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ const start = async () => {
 
     // Menggunakan PORT dari environment atau default ke 5000
     const port = process.env.PORT || 5000;
-    app.listen(port, () => console.log(`Server running on port ${port}`));
+    app.listen(port, '0.0.0.0',() => console.log(`Server running on port ${port}`));
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
