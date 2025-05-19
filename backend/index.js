47e2b7ccc992e6545d154e4821cdf6338d9e174d
Veyzaputri <129712217+Veyzaputri@users.noreply.github.com>
2025-05-19 19:16:39 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,18 +5,33 @@ import dotenv from "dotenv";
 import UserRoute from "./routes/UserRoute.js";
 import NotesRoute from "./routes/NotesRoute.js";
 import sequelize from "./config/Database.js";
+
 dotenv.config();
+
 const app = express();
+
 app.use(cookieParser());
 app.use(cors({
   origin: "http://localhost:3000",
   credentials: true,
 }));
 app.use(express.json());
+
 app.use(UserRoute);
 app.use(NotesRoute);
+
 const start = async () => {
   try {
     await sequelize.authenticate();
     console.log("Database connected");
     await sequelize.sync(); // sinkronisasi model
+
+    // Menggunakan PORT dari environment atau default ke 5000
+    const port = process.env.PORT || 5000;
+    app.listen(port, () => console.log(`Server running on port ${port}`));
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+start();
