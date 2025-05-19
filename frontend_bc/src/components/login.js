2e0486b5b870983cfd4cea76273d9c074561963b
Veyzaputri <129712217+Veyzaputri@users.noreply.github.com>
2025-05-19 23:20:05 +0700

    Update login.js

--- frontend_bc/src/components/login.js
+++ frontend_bc/src/components/login.js
@@ -12,7 +12,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const response = await axios.post(
-        "http://localhost:5000/login",
+        `${BASE_URL}/login`,
         { username, password },
         { withCredentials: true }
       );
